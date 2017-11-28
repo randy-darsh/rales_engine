36af7a495809ee407b9e05e570fe006d85540114
randy-darsh <rspringer47@gmail.com>
2017-11-28 13:48:45 -0700

    created items with request test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128153941) do
+ActiveRecord::Schema.define(version: 20171128201547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20171128153941) do
     t.string "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "unit_price"
+    t.bigint "merchant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -36,4 +46,5 @@ ActiveRecord::Schema.define(version: 20171128153941) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "items", "merchants"
 end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -29,4 +29,15 @@ desc "Import all csv data"
         updated_at: row[4]
       })
     end
-end
+    items = "db/items.csv"
+    CSV.foreach(items, headers: true) do |row|
+      Item.create({
+        name: row[1],
+        description: row[2],
+        unit_price: row[3],
+        merchant_id: row[4],
+        created_at: row[5],
+        updated_at: row[6]
+      })
+    end
+  end
