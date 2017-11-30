ffb8ce63bc5bd645041e627c76dcc2a6851ef418
randy-darsh <rspringer47@gmail.com>
2017-11-29 18:34:49 -0700

    most items sold

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,23 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  default_scope {order(:id)}
+
   def self.most_revenue(x)
-    select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    unscoped.select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
       .joins(invoices: [:invoice_items, :transactions])
       .merge(Transaction.unscoped.successful)
       .group(:id)
       .order("revenue DESC")
+      .limit(x)
+  end
+
+  def self.most_items(x)
+    unscoped.select("items.*, sum(invoice_items.quantity) AS amount_sold")
+      .joins(invoices: [:transactions])
+      .merge(Transaction.unscoped.successful)
+      .group(:id)
+      .order("amount_sold DESC")
       .limit(x)
   end
 end
